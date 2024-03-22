8e0af3ef173cdab02a93aa113afc74446ba2fff6
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2024-03-21 23:10:02 -0600

    Feat:add initial configs/dependencies

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apriProducts: {
+    url: 'https://fakestoreapi.com',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apriProducts: {
+    url: 'https://fakestoreapi.com',
+  },
+};
