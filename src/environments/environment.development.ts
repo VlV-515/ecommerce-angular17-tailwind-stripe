cf78d9bc3ab054e33ed350c716d0c659fdfc3f54
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2024-04-10 01:21:55 -0600

    Feat:Add environments stripe

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,9 @@ export const environment = {
   apriProducts: {
     url: 'https://fakestoreapi.com',
   },
+  stripe: {
+    apiKey:
+      'pk_test_51P3uTJRvZadNamZ5Eg0qTeSjmYWNM4QnW3GbxZxWoWIr3gECU6jkHB6sv5tNCuDJSOCV2ZzpwfEqkzHP2ipCqAdu00etlhdvpX',
+    serverURL: 'http://localhost:4242',
+  },
 };
