f056722d03fcce6ba562f9847e1dafcd4eb10ce8
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2024-04-10 00:00:18 -0600

    Feat:Implement CartStore in Products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { ProductService } from '../../services/products.service';
 import { CardProductComponent } from '../../components/card-product/card-product.component';
+import { CartStore } from '../../services/shopping-cart.store';
+import { ProductModel } from '../../models';
 
 @Component({
   selector: 'app-products',
@@ -10,6 +12,11 @@ import { CardProductComponent } from '../../components/card-product/card-product
   styleUrl: './products.component.scss',
 })
 export default class ProductsComponent {
+  private readonly cartStore = inject(CartStore);
   private readonly productsSvc = inject(ProductService);
   arrProductos = this.productsSvc.arrProducts;
+
+  onAddToCart(product: ProductModel): void {
+    this.cartStore.addtoCart(product);
+  }
 }
