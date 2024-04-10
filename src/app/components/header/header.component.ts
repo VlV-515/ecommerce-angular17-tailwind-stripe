f9825f2dcb442e38c5c1f58dd8961bb5b8703a19
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2024-04-10 00:06:05 -0600

    Fix:Add Routerlink import

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { CartStore } from '../../services/shopping-cart.store';
 import { CurrencyPipe, NgClass, SlicePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SlicePipe, CurrencyPipe, NgClass],
+  imports: [SlicePipe, CurrencyPipe, NgClass, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
