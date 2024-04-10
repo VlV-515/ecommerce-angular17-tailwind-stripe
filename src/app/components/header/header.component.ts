3f98f2589a23e9b1ade1e5d4b0ce4a54c1cbc576
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2024-04-10 00:00:06 -0600

    Feat:Implement CartStore in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CartStore } from '../../services/shopping-cart.store';
+import { CurrencyPipe, NgClass, SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe, CurrencyPipe, NgClass],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  showCart = signal<boolean>(false);
+  cartStore = inject(CartStore);
 }
